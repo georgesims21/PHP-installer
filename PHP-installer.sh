8d5d50bdb4f40dffa8184a9a8189c0d2a1fc06d2
George <georgesims21@hotmail.com>
2021-03-26 13:16:01 +0100

    Cleaned up and commented. Does not work concurrently

--- PHP-installer.sh
+++ PHP-installer.sh
@@ -7,6 +7,7 @@ avail_versions=(8.0 7.4 7.3)
 vernum=$1
 threads=$2
 # Check whether the given version number is included in the avail_versions array
+# https://stackoverflow.com/questions/3685970/check-if-a-bash-array-contains-a-value
 if [[ ! " ${avail_versions[@]} " =~ " ${vernum} " ]]; then
 	echo "Please choose one of the avaliable versions: 8.0, 7.4 or 7.3"
 	exit
@@ -52,7 +53,6 @@ echo "==> Starting build of $phpdir"
 	--with-openssl 		\
 	--with-pear
 make -j"$threads"
-# make test --don't forget this
 make install
 echo "==> Build complete"
 
@@ -63,46 +63,37 @@ phpini="$phpdir"/lib/php.ini
 cp "$phpdir"/php.ini-production "$phpini"
 pecl="$phpdir"/bin/pecl
 pear="$phpdir"/bin/pear
-# Attempted to change pear cache manually, but was unsuccessful
-"$pear" config-set cache_dir /tmp/pear-"$version"/cache
-"$pear" config-set temp_dir /tmp/pear-"$version"/temp
-"$pear" config-set download_dir /tmp/pear-"$version"/download
+"$pear" config-set cache_dir $phpdir/pear-"$version"/cache
+"$pear" config-set temp_dir $phpdir/pear-"$version"/temp
+"$pear" config-set download_dir $phpdir/pear-"$version"/download
 # Link the php-<ver>'s .ini file to its pecl, to allow installation of extensions into this php only
 "$pecl" config-set php_ini "$phpini"
 "$pecl" update-channels
-#"$pecl" upgrade-all
-#if [[ "$vernum" == 7.3 ]]; then
-#  echo "upgrading with xml"
-#	"$pecl" upgrade --force xml_util # if 7.3.0 need to use --force xml_util
-#else
-#  echo "upgrading without xml"
-#	"$pecl" upgrade --force
-#fi
 
 # --- Installing extensions ---
 echo "==> Installing extensions for $phpdir"
 printf "\n" | "$pecl" install raphf
+echo "==> 'Adding extension=raphf.so' to $phpini"
 echo "extension=raphf.so" >> "$phpini"
 # Using PHP 8.0 does not require the user to write "extension=<extension-name>" to the php.ini
 if [[ ! "$vernum" == 8.0 ]]; then
   # Dependency not required in 8.0
   printf "\n" | "$pecl" install propro
+  echo "==> 'Adding extension=propro.so' to $phpini"
   echo "extension=propro.so" >> "$phpini"
 fi
 cd "$phpdir"/bin
 # Create symlink to the PHP executable named php-<ver>
 ln -sT "$phpdir"/bin/php /usr/bin/"$version"
-if [[ "$vernum" == 8.0 ]]; then
-  # Unfortunately memcache could not be installed successfully on < 8.0 due to dependency problems (with memcache-devel)
-  printf "\n" | "$pecl" install memcache
-  echo "extension=memcache.so" >> "$phpini"
-fi
 # printf "\n" | <pecl install> allows to give default values to extensions. This can be improved depending on user needs
 printf "\n" | "$pecl" install oauth
-printf "\n" | "$pecl" install seaslog
-printf "\n" | "$pecl" install swoole
+echo "==> 'Adding extension=oauth.so' to $phpini"
 echo "extension=oauth.so" >> "$phpini"
+printf "\n" | "$pecl" install seaslog
+echo "==> 'Adding extension=seaslog.so' to $phpini"
 echo "extension=seaslog.so" >> "$phpini"
+printf "\n" | "$pecl" install swoole
+echo "==> 'Adding extension=swoole.so' to $phpini"
 echo "extension=swoole.so" >> "$phpini"
 # Different versions of pecl_http are required for 8.0 compared to 7.3 and 7.4
 if [[ "$vernum" == 8.0 ]]; then
@@ -110,14 +101,8 @@ if [[ "$vernum" == 8.0 ]]; then
 else
 	printf "\n" | "$pecl" install pecl_http-3.2.4
 fi
+echo "==> 'Adding extension=http.so' to $phpini"
 echo "extension=http.so" >> "$phpini"
-
-#if [[ ! "$vernum" == 8.0 ]]; then
-#  echo "extension=oauth.so" >> "$phpini"
-#  echo "extension=seaslog.so" >> "$phpini"
-#  echo "extension=swoole.so" >> "$phpini"
-#	echo "extension=http.so" >> "$phpini"
-#fi
 
 # --- Installing Xdebug without PECL ---
 echo "==> Installing Xdebug from source"
@@ -138,6 +123,10 @@ make -j"$threads"
 # Copy the module into the extensions dir for this php-<ver>
 cp "$xdebdir"/modules/xdebug.so "$extdir"
 # Manually write this to the php.ini file
+echo "==> 'Adding zend_extension=$extdir/xdebug.so' to $phpini"
 echo "zend_extension=$extdir/xdebug.so" >> "$phpini"
 echo "==> Install complete"
 echo "==> $version can now be used, e.g. '$ $version -v'"
+# Create copy of php.ini file to allow the next script to check for changes
+mkdir "$phpdir"/.config
+cp "$phpini" "$phpdir"/.config/php.ini_backup
